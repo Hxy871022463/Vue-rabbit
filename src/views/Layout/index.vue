ef708526949013a4dc112ef1c3429dfba39c27b1
Hxy871022463 <871022463@qq.com>
2025-08-19 23:22:23 +0800

    pinia优化导航数据

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -5,9 +5,19 @@ import LayoutHeader from './components/LayoutHeader.vue'
 import LayoutFooter from './components/LayoutFooter.vue'
 import LayoutFixed from './components/LayoutFixed.vue'
 
+import { useCategoryStore } from '@/stores/category'
+import { onMounted } from 'vue'
+
+const categoryStore = useCategoryStore()
+
+onMounted(() => {
+  categoryStore.getCategory()
+})
+
 </script>
 
 <template>
+
   <LayoutFixed />
   <LayoutNav />
   <LayoutHeader />
